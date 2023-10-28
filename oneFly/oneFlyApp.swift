859a9ecf71552f0f68b24cea4f567c90dbe97c01
werton silva <mr.werton@gmail.com>
2023-10-28 01:28:33 -0300

    Initial Commit

---
+++ oneFly/oneFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneFlyApp.swift
+//  oneFly
+//
+//  Created by Werton Silva on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct oneFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
